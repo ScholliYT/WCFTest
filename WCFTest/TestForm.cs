99690b9e20a7116dd7b4ed96ea5ffaa34f9fb46d
Scholli <scholliyt@gmail.com>
2017-08-08 00:45:15 +0200

    Test UI

--- WCFTest/TestForm.cs
+++ WCFTest/TestForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WCFTest.ServiceReference1;
 
 namespace WCFTest
 {
@@ -15,6 +16,42 @@ namespace WCFTest
         public TestForm()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.TestServiceClient client = new ServiceReference1.TestServiceClient();
+
+            double answer;
+            answer = client.plus(txbWert1.Text, txbWert2.Text);
+            lblOutput.Text = answer;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.TestServiceClient client = new ServiceReference1.TestServiceClient();
+
+            double answer;
+            answer = client.minus(txbWert1.Text, txbWert2.Text);
+            lblOutput.Text = answer;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.TestServiceClient client = new ServiceReference1.TestServiceClient();
+
+            double answer;
+            answer = client.mal(txbWert1.Text, txbWert2.Text);
+            lblOutput.Text = answer;
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.TestServiceClient client = new ServiceReference1.TestServiceClient();
+
+            double answer;
+            answer = client.geteilt(txbWert1.Text, txbWert2.Text);
+            lblOutput.Text = answer;
         }
     }
 }
