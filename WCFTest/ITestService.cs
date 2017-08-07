7b30b35be5e91e7d350c573e4bb576cbfcf778a5
Scholli <scholliyt@gmail.com>
2017-08-07 23:58:54 +0200

    initial

---
+++ WCFTest/ITestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFTest
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Schnittstellennamen "ITestService" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    [ServiceContract]
+    public interface ITestService
+    {
+        [OperationContract]
+        int getDoubledValue(int value);
+    }
+}
---
+++ WCFTest/TestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFTest
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Klassennamen "TestService" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    public class TestService : ITestService
+    {
+        public int getDoubledValue(int value)
+        {
+            return value*2;
+        }
+    }
+}
