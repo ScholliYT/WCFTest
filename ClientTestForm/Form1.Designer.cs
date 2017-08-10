2b080d26536409c373f859fc55ece12ab51bede6
Nico Kranz <kranznico@live.de>
2017-08-10 13:45:46 +0200

    nicos cooles design

--- ClientTestForm/Form1.Designer.cs
+++ ClientTestForm/Form1.Designer.cs
@@ -39,7 +39,11 @@
             this.btnMultiply = new System.Windows.Forms.Button();
             this.btnDivide = new System.Windows.Forms.Button();
             this.lblOutput = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -58,8 +62,8 @@
             this.tableLayoutPanel1.Controls.Add(this.btnMultiply, 2, 2);
             this.tableLayoutPanel1.Controls.Add(this.btnDivide, 2, 3);
             this.tableLayoutPanel1.Controls.Add(this.lblOutput, 1, 4);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 39);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
@@ -67,7 +71,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(270, 147);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(286, 147);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label1
@@ -85,7 +89,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(138, 5);
+            this.label2.Location = new System.Drawing.Point(146, 5);
             this.label2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 13);
@@ -99,17 +103,17 @@
             this.txbWert1.Margin = new System.Windows.Forms.Padding(5);
             this.txbWert1.Multiline = true;
             this.txbWert1.Name = "txbWert1";
-            this.txbWert1.Size = new System.Drawing.Size(105, 19);
+            this.txbWert1.Size = new System.Drawing.Size(113, 19);
             this.txbWert1.TabIndex = 2;
             // 
             // txbWert2
             // 
             this.txbWert2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txbWert2.Location = new System.Drawing.Point(140, 34);
+            this.txbWert2.Location = new System.Drawing.Point(148, 34);
             this.txbWert2.Margin = new System.Windows.Forms.Padding(5);
             this.txbWert2.Multiline = true;
             this.txbWert2.Name = "txbWert2";
-            this.txbWert2.Size = new System.Drawing.Size(105, 19);
+            this.txbWert2.Size = new System.Drawing.Size(113, 19);
             this.txbWert2.TabIndex = 3;
             // 
             // btnAdd
@@ -118,7 +122,7 @@
             this.btnAdd.Location = new System.Drawing.Point(25, 63);
             this.btnAdd.Margin = new System.Windows.Forms.Padding(5);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(105, 19);
+            this.btnAdd.Size = new System.Drawing.Size(113, 19);
             this.btnAdd.TabIndex = 4;
             this.btnAdd.Text = "+";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -130,7 +134,7 @@
             this.btnMinus.Location = new System.Drawing.Point(25, 92);
             this.btnMinus.Margin = new System.Windows.Forms.Padding(5);
             this.btnMinus.Name = "btnMinus";
-            this.btnMinus.Size = new System.Drawing.Size(105, 19);
+            this.btnMinus.Size = new System.Drawing.Size(113, 19);
             this.btnMinus.TabIndex = 5;
             this.btnMinus.Text = "-";
             this.btnMinus.UseVisualStyleBackColor = true;
@@ -139,10 +143,10 @@
             // btnMultiply
             // 
             this.btnMultiply.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnMultiply.Location = new System.Drawing.Point(140, 63);
+            this.btnMultiply.Location = new System.Drawing.Point(148, 63);
             this.btnMultiply.Margin = new System.Windows.Forms.Padding(5);
             this.btnMultiply.Name = "btnMultiply";
-            this.btnMultiply.Size = new System.Drawing.Size(105, 19);
+            this.btnMultiply.Size = new System.Drawing.Size(113, 19);
             this.btnMultiply.TabIndex = 6;
             this.btnMultiply.Text = "*";
             this.btnMultiply.UseVisualStyleBackColor = true;
@@ -151,10 +155,10 @@
             // btnDivide
             // 
             this.btnDivide.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnDivide.Location = new System.Drawing.Point(140, 92);
+            this.btnDivide.Location = new System.Drawing.Point(148, 92);
             this.btnDivide.Margin = new System.Windows.Forms.Padding(5);
             this.btnDivide.Name = "btnDivide";
-            this.btnDivide.Size = new System.Drawing.Size(105, 19);
+            this.btnDivide.Size = new System.Drawing.Size(113, 19);
             this.btnDivide.TabIndex = 7;
             this.btnDivide.Text = "/";
             this.btnDivide.UseVisualStyleBackColor = true;
@@ -170,12 +174,59 @@
             this.lblOutput.TabIndex = 8;
             this.lblOutput.Text = "Output: ";
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 33);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkRed;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(253, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(220, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "_";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(270, 147);
+            this.ClientSize = new System.Drawing.Size(286, 186);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(286, 186);
             this.Name = "Form1";
@@ -183,6 +234,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -199,6 +251,9 @@
         private System.Windows.Forms.Button btnMultiply;
         private System.Windows.Forms.Button btnDivide;
         private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ClientTestForm/Form1.cs
+++ ClientTestForm/Form1.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Windows.Forms;
 using ClientTestForm.WCFServer;
+using System.Runtime.InteropServices;
 
 namespace ClientTestForm
 {
     public partial class Form1 : Form
     {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        const int WS_MINIMIZEBOX = 0x20000;
+        const int CS_DBLCLKS = 0x8;
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams; cp.Style |= WS_MINIMIZEBOX; cp.ClassStyle |= CS_DBLCLKS; return cp;
+            }
+        }
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
         public Form1()
         {
             InitializeComponent();
@@ -49,6 +68,33 @@ namespace ClientTestForm
 
         private void label1_Click(object sender, EventArgs e)
         {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+        public void mouseMove(MouseEventArgs e)
+        {
+            if (e.Button != System.Windows.Forms.MouseButtons.Left) return;
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_NCLBUTTONDOWN, new IntPtr(HT_CAPTION), IntPtr.Zero);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseMove(e);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseMove(e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
         }
     }
 }
